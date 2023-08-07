4ce466a47958bb22fe9490a5c18e84e8ba67ea0d
Maksim Nikolay <mvnikolay@gmail.com>
2023-08-07 23:06:12 +0300

    add func to add expense

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime
 import yaml
+import re
 
 import telebot
 from telebot import types
@@ -59,79 +60,67 @@ def first_choice(message):
     else:
         bot.send_message(chat_id, 'Неизвестная команда, возвращаю в главное меню')
         print(f'Unknown command, back to main menu')
-    getting_started()
+        getting_started()
 
 
 def add_expense():
     ikm = types.InlineKeyboardMarkup(row_width=2)
-    button1 = types.InlineKeyboardButton('Продукты', callback_data='add_category', category='продукты')
-    button2 = types.InlineKeyboardButton('Развлечения', callback_data='add_category', category='развлечения')
-    button3 = types.InlineKeyboardButton('Рестораны', callback_data='add_category', category='рестораны')
-    button4 = types.InlineKeyboardButton('Аптеки', callback_data='add_category', category='аптеки')
-    button5 = types.InlineKeyboardButton('Одежда', callback_data='add_category', category='одежда')
-    button6 = types.InlineKeyboardButton('Дом', callback_data='add_category', category='дом')
-    button7 = types.InlineKeyboardButton('Транспорт', callback_data='add_category', category='транспорт')
-    button8 = types.InlineKeyboardButton('Прочее', callback_data='add_category', category='прочее')
+    button1 = types.InlineKeyboardButton('Продукты', callback_data='add_category_products')
+    button2 = types.InlineKeyboardButton('Развлечения', callback_data='add_category_entertainments')
+    button3 = types.InlineKeyboardButton('Рестораны', callback_data='add_category_restaurant')
+    button4 = types.InlineKeyboardButton('Аптеки', callback_data='add_category_pharmacy')
+    button5 = types.InlineKeyboardButton('Одежда', callback_data='add_category_clothes')
+    button6 = types.InlineKeyboardButton('Дом', callback_data='add_category_home')
+    button7 = types.InlineKeyboardButton('Транспорт', callback_data='add_category_transport')
+    button8 = types.InlineKeyboardButton('Прочее', callback_data='add_category_other')
     ikm.add(button1, button2, button3, button4, button5, button6, button7, button8)
     bot.send_message(chat_id, 'Выбери категорию', reply_markup=ikm)
 
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_data(call):
+    if 'add_category' in call.data:
+        category = call.data.rpartition('_')[2]
+        add_category(category)
+    elif 'add_expense_with_date' in call.data:
+        def_dic = call.data.replace('add_expense_with_date_', '')
+        category = def_dic.partition('_')[0]
+        exp_sum, _, exp_date = def_dic.partition('_')[2].partition('_')
+        if not exp_date:
+            get_date(category=category, exp_sum=exp_sum)
+        add_expense_with_date(category=category, exp_sum=exp_sum, exp_date=exp_date)
+
+
 def add_category(category: str):
-    message = bot.send_message('Введи сумму в формате 1999.99')
+    message = bot.send_message(chat_id, 'Введи сумму в формате 1999.99')
     bot.register_next_step_handler(message, add_date, category=category)
 
 
 def add_date(message, category):
     ikm = types.InlineKeyboardMarkup(row_width=2)
-    button1 = types.InlineKeyboardButton('Оставить текущий день', callback_data='add_expense_with_date',
-                                         category=category, exp_sum=message.text, exp_date=datetime.date.today())
+    button1 = types.InlineKeyboardButton(
+        'Оставить текущий день', callback_data=f'add_expense_with_date_{category}_{message.text}_{datetime.date.today()}')
     button2 = types.InlineKeyboardButton(
-        'Ввести дату', callback_data='get_date', category=category, exp_sum=message.text)
+        'Ввести дату', callback_data=f'add_expense_with_date_{category}_{message.text}')
     ikm.add(button1, button2)
     bot.send_message(chat_id, 'Хотите ввести произвольную дату?', reply_markup=ikm)
 
 
 def get_date(category: str, exp_sum: str):
     message = bot.send_message(chat_id, 'Введи дату в формате 1999-12-31')
+    date_format = '[\d]{4}[.,-/][\d]{2}[.,-/][\d]{2}'
+    if not re.fullmatch(date_format, message.text):
+        message = bot.send_message(chat_id, 'Неправильный формат даты, введи заново')
+        bot.register_next_step_handler(message, get_date, category=category, exp_sum=exp_sum)
     exp_date = message.text
+    add_expense_with_date(category=category, exp_sum=exp_sum, exp_date=exp_date)
 
 
-
-def add_full_expense(message, category, date):
-    expense = message.text
-
-
-
-def add_category_products():
-    add_category('продукты')
-
-
-def add_category_entertainments():
-    add_category('развлечения')
-
-
-def add_category_restaurants():
-    add_category('рестораны')
-
-
-def add_category_pharmacies():
-    add_category('аптеки')
-
-
-def add_category_clothes():
-    add_category('одежда')
-
-
-def add_category_home():
-    add_category('дом')
-
-
-def add_category_transport():
-    add_category('транспорт')
-
-
-def add_category_other():
-    add_category('прочее')
+def add_expense_with_date(category, exp_sum, exp_date):
+    expense = {'user': known_users.get(user_id), 'category': category, 'sum': exp_sum, 'report_date': exp_date}
+    expenses_book.add_expense(expense)
+    bot.send_message(chat_id, 'Покупка добавлена')
+    getting_started()
 
 
 def get_statistics():
