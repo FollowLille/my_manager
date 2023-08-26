45965a211f2a4f2387beb1dd5b936930d332b16e
Maksim Nikolay <mvnikolay@gmail.com>
2023-08-26 21:15:51 +0300

    delete unnecessary library from requirements

--- users_configs/users_config.py
+++ users_configs/users_config.py
@@ -10,27 +10,27 @@ class UsersLib:
         known_users = cls.get_users()
         known_users[user_id]["name"] = name
         known_users[user_id]["vision"] = 'my'
-        with open('users_configs/users.json', 'w', encoding='utf-8') as file:
+        with open('my_manager/users_configs/users.json', 'w', encoding='utf-8') as file:
             json.dump(known_users, file, ensure_ascii=False)
 
     @staticmethod
     def get_users():
-        with open('users_configs/users.json', 'r', encoding='utf-8') as file:
+        with open('my_manager/users_configs/users.json', 'r', encoding='utf-8') as file:
             known_users = json.load(file)
         return known_users
 
     @classmethod
     def change_name(cls, user_id: int, new_name: str):
-        with open('users_configs/users.json', 'r', encoding='utf-8') as file:
+        with open('my_manager/users_configs/users.json', 'r', encoding='utf-8') as file:
             known_users = json.load(file)
         known_users[f'{user_id}']['name'] = new_name
-        with open('users_configs/users.json', 'w', encoding='utf-8') as file:
+        with open('my_manager/users_configs/users.json', 'w', encoding='utf-8') as file:
             json.dump(known_users, file, ensure_ascii=False)
 
     @classmethod
     def change_vision(cls, user_id: int, rule: str):
-        with open('users_configs/users.json', 'r', encoding='utf-8') as file:
+        with open('my_manager/users_configs/users.json', 'r', encoding='utf-8') as file:
             known_users = json.load(file)
         known_users[f'{user_id}']['vision'] = rule
-        with open('users_configs/users.json', 'w', encoding='utf-8') as file:
+        with open('my_manager/users_configs/users.json', 'w', encoding='utf-8') as file:
             json.dump(known_users, file, ensure_ascii=False)
